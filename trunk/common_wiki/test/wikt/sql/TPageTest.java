a50717bca43a03cdd0cca5bd2d90ff5a40b8672e
andrew.krizhanovsky <andrew.krizhanovsky@2f93c6a6-ea4c-11dd-9ee4-63b70bd76fba>
2009-02-04 15:12:48 +0000

    TPage test get and insert

--- trunk/common_wiki/test/wikt/sql/TPageTest.java
+++ trunk/common_wiki/test/wikt/sql/TPageTest.java
@@ -1,7 +1,6 @@
 
 package wikt.sql;
 
-import java.sql.Connection;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -13,7 +12,7 @@ import wikipedia.sql.Connect;
 
 public class TPageTest {
 
-    public Connect   ruwikt_conn;
+    public Connect   ruwikt_parsed_conn;
 
     public TPageTest() {
     }
@@ -28,8 +27,8 @@ public class TPageTest {
 
     @Before
     public void setUp() {
-        ruwikt_conn = new Connect();
-        ruwikt_conn.Open(Connect.RUWIKT_HOST,Connect.RUWIKT_DB,Connect.RUWIKT_USER,Connect.RUWIKT_PASS);
+        ruwikt_parsed_conn = new Connect();
+        ruwikt_parsed_conn.Open(Connect.RUWIKT_HOST,Connect.RUWIKT_PARSED_DB,Connect.RUWIKT_USER,Connect.RUWIKT_PASS);
     }
 
     @After
@@ -43,10 +42,10 @@ public class TPageTest {
     public void testInsert() {
         System.out.println("insert_ru");
         String page_title;
-        java.sql.Connection conn = ruwikt_conn.conn;
+        Connect conn = ruwikt_parsed_conn;
 
         //page_title = "test_тыблоко";
-        page_title = ruwikt_conn.enc.EncodeFromJava("test_тыблоко");
+        page_title = ruwikt_parsed_conn.enc.EncodeFromJava("test_тыблоко");
         int word_count = 7;
         int wiki_link_count = 13;
         boolean is_in_wiktionary = true;
