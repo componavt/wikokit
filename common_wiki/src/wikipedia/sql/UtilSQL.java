6d65b56a6288f37ed92193919fe3f683580aad34
andrew.krizhanovsky <andrew.krizhanovsky@2f93c6a6-ea4c-11dd-9ee4-63b70bd76fba>
2009-03-18 15:15:40 +0000

    + small test change

--- common_wiki/src/wikipedia/sql/UtilSQL.java
+++ common_wiki/src/wikipedia/sql/UtilSQL.java
@@ -1,23 +1,19 @@
-/* UtilSQL.java - misc SQL operations.
- *
- * Copyright (c) 2009 Andrew Krizhanovsky <andrew.krizhanovsky at gmail.com>
- * Distributed under GNU Public License.
- */
 
 package wikipedia.sql;
 
+import wikipedia.sql.Connect;
 import java.sql.*;
 
-/** Miscellaneous SQL operations.
+/** Misc SQL routines.
  */
 public class UtilSQL {
 
     /** Deletes all records from the table 'table_name', resets auto increment.
      *
-     * DELETE FROM lang;
-     * ALTER TABLE lang AUTO_INCREMENT = 0;
+     * DELETE FROM table_name;
+     * ALTER TABLE table_name AUTO_INCREMENT = 0;
      */
-    public static void deleteAllRecordsResetAutoIncrement (Connect connect,String table_name) {
+    public static void deleteAllRecordsResetAutoIncrement (Connect connect, String table_name) {
 
         Statement   s = null;
         ResultSet   rs= null;
@@ -29,12 +25,11 @@ public class UtilSQL {
             s.executeBatch();
 
         } catch(SQLException ex) {
-            System.err.println("SQLException (wikt_parsed UtilSQL.java deleteAllRecordsResetAutoIncrement()):: table name is "+ table_name +"; " + ex.getMessage());
+            System.err.println("SQLException (wikipedia.sql UtilSQL.java deleteAllRecordsResetAutoIncrement()):: table = "+ table_name +"; msg = " + ex.getMessage());
         } finally {
             if (rs != null) {   try { rs.close(); } catch (SQLException sqlEx) { }  rs = null; }
             if (s != null)  {   try { s.close();  } catch (SQLException sqlEx) { }  s = null;  }
         }
     }
 
-    
 }
