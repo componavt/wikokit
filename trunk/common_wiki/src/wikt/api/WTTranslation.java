05723ba8b6071f61a42af092ec2f5a4cb02e10e7
andrew.krizhanovsky <andrew.krizhanovsky@2f93c6a6-ea4c-11dd-9ee4-63b70bd76fba>
2009-04-26 19:17:19 +0000

    + wikt.api

---
+++ trunk/common_wiki/src/wikt/api/WTTranslation.java
@@ -0,0 +1,28 @@
+/* WTTranslation.java - high-level functions for translation by Wiktionary.
+ *
+ * Copyright (c) 2009 Andrew Krizhanovsky <andrew.krizhanovsky at gmail.com>
+ * Distributed under GNU General Public License.
+ */
+
+package wikt.api;
+
+import wikipedia.language.LanguageType;
+import wikipedia.sql.Connect;
+
+/** High-level functions for translation by Wiktionary.
+ *
+ * @see wikt.word.WTranslation
+ * @see wikt.sql.TTranslation
+ */
+public class WTTranslation {
+
+    /** Translates the word from source to target language. */
+    public static String [] translate (Connect connect,
+            LanguageType source_lang,LanguageType target_lang,String word) {
+
+        
+        
+        return null;
+    }
+
+}
