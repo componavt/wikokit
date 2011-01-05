183eb2b12a81c4af5b127fa1cae6ce858b6bdf76
andrew.krizhanovsky <andrew.krizhanovsky@2f93c6a6-ea4c-11dd-9ee4-63b70bd76fba>
2011-01-05 13:45:01 +0000

    + lang codes3

--- trunk/common_wiki/src/wikipedia/language/LanguageType.java
+++ trunk/common_wiki/src/wikipedia/language/LanguageType.java
@@ -1,10 +1,3 @@
-// current: Translingual
-// list: http://en.wiktionary.org/wiki/Special:Statistics
-
-// 
-// it is 
-// current: 01.log  unknown language: Woiwurrung
-// http://en.wiktionary.org/wiki/Wiktionary:Index_to_templates/languages#Template_table
 /*
  * LanguageType.java - code of languages in wiki.
  * 
@@ -13,6 +6,7 @@
  */
 
 //http://ru.wiktionary.org/w/index.php?title=%D0%A8%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD:%D0%BF%D0%B5%D1%80%D0%B5%D0%B2-%D0%B1%D0%BB%D0%BE%D0%BA&diff=next&oldid=1243557
+// http://en.wiktionary.org/wiki/Wiktionary:Index_to_templates/languages#Template_table
 
 package wikipedia.language;
 
