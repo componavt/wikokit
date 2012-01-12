8df3b11df9a1b3f16f8b5ea6dde6fc10898b3d4d
andrew.krizhanovsky <andrew.krizhanovsky@2f93c6a6-ea4c-11dd-9ee4-63b70bd76fba>
2012-01-12 14:37:00 +0000

    release wiwordik 0.09.1094

--- wiwordik/sqlite/run_jarsigner.bat
+++ wiwordik/sqlite/run_jarsigner.bat
@@ -1,7 +1,7 @@
 ::
 SET version=0.09
-::SET sqlitefile=enwikt20110618
-SET sqlitefile=ruwikt20110521
+SET sqlitefile=enwikt20111008
+::SET sqlitefile=ruwikt20110521
 ::
 SET jarfile=wiwordik-%version%-%sqlitefile%.jar
 cd ..
@@ -11,6 +11,3 @@ cd ..
 ::
 :: see http://wiki.plexinfo.net/index.php?title=How_to_sign_JAR_files
 "C:\Program Files\Java\jdk1.7.0_02\bin\jarsigner" -keystore D:\all\docs\my_texts\social\root\jarsigner\myKeystore store/%jarfile% andrew_k
-"C:\Program Files\Java\jdk1.7.0_02\bin\jarsigner" -keystore D:\all\docs\my_texts\social\root\jarsigner\myKeystore ../jnlp/lib/common_wiki.jar andrew_k
-"C:\Program Files\Java\jdk1.7.0_02\bin\jarsigner" -keystore D:\all\docs\my_texts\social\root\jarsigner\myKeystore ../jnlp/lib/ruwikt20110521.sqlite.jar andrew_k
-"C:\Program Files\Java\jdk1.7.0_02\bin\jarsigner" -keystore D:\all\docs\my_texts\social\root\jarsigner\myKeystore ../jnlp/lib/sqlite-jdbc-3.7.2.jar andrew_k
