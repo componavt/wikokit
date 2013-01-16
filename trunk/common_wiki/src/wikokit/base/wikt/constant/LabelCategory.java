970eae3c6504b42ee1ee1a0877473bceb1da6c8d
andrew.krizhanovsky <andrew.krizhanovsky@2f93c6a6-ea4c-11dd-9ee4-63b70bd76fba>
2013-01-16 17:26:13 +0000

    + LabelCategory.java

---
+++ trunk/common_wiki/src/wikokit/base/wikt/constant/LabelCategory.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wikokit.base.wikt.constant;
+
+/**
+ *
+ * @author andrew
+ */
+public class LabelCategory {
+    
+}
