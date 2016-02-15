eeeda38ac1b99136626c445d164576ad5191a07a
componavt <andrew.krizhanovsky@gmail.com>
2016-02-15 18:49:57 +0300

    test cola again
    test cola2

--- run_wiwordik.bat
+++ run_wiwordik.bat
@@ -1,4 +1,4 @@
 cd wiwordik
 wiwordik.bat
 cd ..
-:: javafx -cp ./wiwordik/dist/wiwordik.jar;./common_wiki/lib/mysql-connector-java-5.1.7-bin.jar;./common_wiki/lib/sqlite-jdbc-3.6.17.1.jar;./common_wiki/dist/common_wiki.jar wiwordik.Main test git-cola
+:: javafx -cp ./wiwordik/dist/wiwordik.jar;./common_wiki/lib/mysql-connector-java-5.1.7-bin.jar;./common_wiki/lib/sqlite-jdbc-3.6.17.1.jar;./common_wiki/dist/common_wiki.jar wiwordik.Main test git-cola 2
